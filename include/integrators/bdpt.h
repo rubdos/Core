374469bab993c3f7ee7127b83008d04edb08381a
povmaniaco <p.alcaide@hotmail.com>
2014-05-05 07:55:49 +0200

    Small mismatch typo on BDPT_H guard

--- include/integrators/bdpt.h
+++ include/integrators/bdpt.h
@@ -14,7 +14,7 @@
 *		License along with this library; if not, write to the Free Software
 *		Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
-#ifndef BDTP_H
+#ifndef BDPT_H
 #define BDPT_H
 
 #include <core_api/environment.h>
