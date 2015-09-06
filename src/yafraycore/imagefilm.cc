5d8eea30f0777c6cacdf229fbf9c9c571a839b2f
povmaniac <thebountyrenderer@gmail.com>
2015-09-06 13:18:18 +0200

    Add license text to some files.

--- src/yafraycore/imagefilm.cc
+++ src/yafraycore/imagefilm.cc
@@ -117,7 +117,7 @@ float Lanczos2(float dx, float dy)
 
 //-
 imageFilm_t::imageFilm_t (int width, int height, int xstart, int ystart, colorOutput_t &out, float filterSize,
-                          filterType filt,renderEnvironment_t *e, bool showSamMask,int tSize,
+                          filterType filt, renderEnvironment_t *e, bool showSamMask, int tSize,
                           imageSpliter_t::tilesOrderType tOrder, bool pmA, bool drawParams):
 flags(0), w(width), h(height), cx0(xstart), cy0(ystart), gamma(1.0), filterw(filterSize*0.5), output(&out),
 clamp(false), split(true), interactive(true), abort(false), correctGamma(false), splitter(0), pbar(0),
